ffb4e754831b30743fb80f8f1843a3610c873801
Rafnes <rafzograf@gmail.com>
2024-09-19 16:47:34 +0300

    hw done

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -2,6 +2,7 @@ package pro.sky.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.calculator.service.CalculatorService;
 
@@ -17,5 +18,29 @@ public class CalculatorController {
     @GetMapping
     public String welcome() {
         return calculatorService.welcome();
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Integer num1,
+                       @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Integer num1,
+                        @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.minus(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Integer num1,
+                           @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.multiply(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Integer num1,
+                         @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.divide(num1, num2);
     }
 }
